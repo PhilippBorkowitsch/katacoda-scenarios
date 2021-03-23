e3bde32fb82a9adb3fe6d98945a90a8fc9cf2349
PhilippBorkowitsch <pborkowitsch@gmail.com>
2021-03-23 01:18:15 +0100

    yeahhh now it works

--- star-schema/startup.sh
+++ star-schema/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -dp 5342:5432 -e POSTGRES_PASSWORD=test123 -it pbork/katacoda-starschema
+docker run -dp 5342:5432 -it pbork/katacoda-starschema -e POSTGRES_PASSWORD=test123 
